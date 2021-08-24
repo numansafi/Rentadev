2ea90811ab944f1a25b462ca2ea7274d5ee7cd36
Max McLellan <mclellanmax@gmail.com>
2021-08-24 11:43:42 +0100

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'John Lithgow',
+  email: 'john.lithgow@gmail.com'
+)
+
+User.create!(
+  name: 'Adam Armstrong',
+  email: 'adam.armstrong@gmail.com'
+)
+
+User.create!(
+  name: 'Fraser Forster',
+  email: 'fraser.forster@gmail.com'
+)
+
+User.create!(
+  name: 'Ellie Sharp',
+  email: 'ellie.sharp@gmail.com'
+)
+
+User.create!(
+  name: 'Dan Theman',
+  email: 'dan.theman@gmail.com'
+)
+
+User.create!(
+  name: 'Liam Neeson',
+  email: 'liam.neeson@gmail.com'
+)
+
+User.create!(
+  name: 'James McAvoy',
+  email: 'james.mcavoy@gmail.com'
+)
+
+User.create!(
+  name: 'Harry Trotter',
+  email: 'harry.trotter@gmail.com'
+)
