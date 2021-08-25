40f47fd3827ee90ffc07b3871d5cb5f64b548cb5
Numan Safi <numansafi@live.com>
2021-08-25 13:29:19 +0100

    booking controller - added index/show methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, :only [:show]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_booking
+    @booking = booking.find(params[:id])
+  end
 end
