da194c7ff6acff1c9aa976bfe3a32fc0d05bbf2f
Numan Safi <numansafi@live.com>
2021-08-25 14:36:26 +0100

    Added booking index and show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
 
-  before_action :set_booking, :only [:show]
+  before_action :set_booking, only: [:show]
 
   def index
     @bookings = Booking.all
@@ -9,9 +9,31 @@ class BookingsController < ApplicationController
   def show
   end
 
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.listing = @listing
+    @listing.user = current_user
+    if @booking.save
+      redirect_to listings_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_booking
-    @booking = booking.find(params[:id])
+    @booking = Booking.find(params[:id])
+  end
+
+
+
+  def booking_params
+    params.require(:booking).permit(:date)
   end
 end
