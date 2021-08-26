4aeae43e8e4ebdd65d60785804fe5c1dcbf98c9b
Scott Johnson <scottmacaulayjohnson@googlemail.com>
2021-08-26 13:09:12 +0100

    bookings edit delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :listings do
-    resources :bookings
+    resources :bookings, except: [:destroy]
   end
-  
 
+resources :bookings, only: [:destroy]
 
 end
